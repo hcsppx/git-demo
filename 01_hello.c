5897d372600b3e32c45004d26f45f3ade980f115
hcs <hcs@foxmail.com>
2022-07-04 14:25:40 +0800

    my first commit

---
+++ 01_hello.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	printf("hello word!\n");
+	printf("hello word!\n");
+	printf("hello word!\n");
+	printf("hello word!\n");
+	printf("hello word!\n");
+	printf("hello word!\n");
+	printf("hello word!\n");
+	printf("hello word!\n");
+	return 0;
+}

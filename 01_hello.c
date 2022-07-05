d0b7cad819bf9e9469971754f2e5ea07a9f8a68f
hcs <hcs@foxmail.com>
2022-07-05 15:14:41 +0800

    master test

--- 01_hello.c
+++ 01_hello.c
@@ -7,7 +7,7 @@ int main()
 	printf("hello word!\n");
 	printf("hello word!\n");
 	printf("hello word!\n");
-	printf("hello word!\n");
+	printf("hello word!\n");master test
 	printf("hello word!\n");
 	hot_fix;
 	return 0;
